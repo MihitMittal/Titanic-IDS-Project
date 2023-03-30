14eb02798408b8b3dff259fcfaffdbdce140d9f6
MihitMittal <mihitmittal@gmail.com>
2023-03-30 12:10:30 +0530

    imported datasets

--- Project.py
+++ Project.py
@@ -1,5 +1,13 @@
 #%%
-#Imports
+#Importing libraries
 import numpy as np
 import pandas as pd
-import sklearn
+import sklearn.metrics
+# %%
+#Importing datasets
+testing_ds = pd.read_csv("titanic_test.csv")
+training_ds = pd.read_csv("titanic_train.csv")
+# %%
+#View the top few rows of training dataset
+training_ds.head()
+# %%
